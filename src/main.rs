4c1acf8359540ef45210e9dffebca8ad18b42134
Daniel Käsemodel <daniel.kasemodel@gmail.com>
2021-05-25 14:29:59 -0300

    Creata a constant
    Signed-off-by: Daniel Käsemodel <daniel.kasemodel@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,7 @@ fn main() {
     println!("Value of X: {}", x);
     x = 6;
     println!("Value of X: {}", x);
+
+    const MAX_VALUE: u32 = 100_000;
+    println!("Constant value: {}", MAX_VALUE);
 }
